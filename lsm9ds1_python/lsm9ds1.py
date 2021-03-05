00207b6618b654a639c35c98a13cb4c423a0d43d
electron001 <mbiskup123@gmail.com>
2021-03-05 08:53:53 +0000

    __init__.py

--- lsm9ds1_python/lsm9ds1.py
+++ lsm9ds1_python/lsm9ds1.py
@@ -5,7 +5,7 @@ import smbus
 import time
 
 # Accelerometr default slave address
-AccelerometrSlaveAddr = 0x1E
+AccelerometerSlaveAddr = 0x1E
 # Accelerometer ID
 AccelerometerID = 0x68
 
@@ -23,11 +23,11 @@ WHO_AM_I_M = 0x0F
 
 bus = smbus.SMBus(1)
 
-class LSM9DS1:
+class lsm9ds1:
 
-def searchDevice(self):
-	who_am_i = bus.read_byte_data(self.address, WHO_AM_I)
-	if(who_am_i == AccelerometerID):
-		return true
-	else:
-		return false
+	def searchDevice(self):
+		who_am_i = bus.read_byte_data(self.address, WHO_AM_I)
+		if(who_am_i == AccelerometerID):
+			return true
+		else:
+			return false
